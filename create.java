797efa21bec0df4bdfcae084ef583b203e898596
CodeAldous275 <aldousdsouza2@gmail.com>
2024-11-17 20:44:46 +0530

    Updated create.java and employee.java

--- create.java
+++ create.java
@@ -2,17 +2,27 @@ import java.util.*;
 public class create{
 	public static void main(String[] args){
 		ArrayList<student> stud = new ArrayList<>();
-
+		ArrayList<employee> emp = new ArrayList<>();
 		System.out.print("Enter the no. of students:");
 		int n = sc.nextInt();
+		System.out.print("Enter the no. of employees");
+		int m = sc.nextInt();
 
 		for(int i = 0 ; i <= n - 1 ; i++){
 			stud.add(new student("Aldous",20,"Male","Science","Computer Science",2202301);
 
 		}
 
+		for(int i = 0 ; i <= m - 1 ; i++){
+			emp.add(new employee("Shreyash",20,"Male",101,29000);
+		}
+
 		for(student st : stud){
 			st.display();
+		}
+
+		for(employee e : emp){
+			e.display();
 		}
 	}
 }
