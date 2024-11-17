fc10acfa4d27a7c2efcdff6961ca39d657b55e33
Aldocool275 <121561781+Aldocool275@users.noreply.github.com>
2024-11-17 21:14:26 +0530

    Update create.java

--- create.java
+++ create.java
@@ -1,28 +1,35 @@
 import java.util.*;
 public class create{
 	public static void main(String[] args){
-		ArrayList<student> stud = new ArrayList<>();
-		ArrayList<employee> emp = new ArrayList<>();
-		System.out.print("Enter the no. of students:");
-		int n = sc.nextInt();
-		System.out.print("Enter the no. of employees");
-		int m = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		try{
+			ArrayList<student> stud = new ArrayList<>();
+			ArrayList<employee> emp = new ArrayList<>();
+			System.out.print("Enter the no. of students:");
+			int n = sc.nextInt();
+			System.out.print("Enter the no. of employees");
+			int m = sc.nextInt();
 
-		for(int i = 0 ; i <= n - 1 ; i++){
-			stud.add(new student("Aldous",20,"Male","Science","Computer Science",2202301);
+			for(int i = 0 ; i <= n - 1 ; i++){
+				stud.add(new student("Aldous",20,"Male","Science","Computer Science",2202301);
 
-		}
+			}
 
-		for(int i = 0 ; i <= m - 1 ; i++){
-			emp.add(new employee("Shreyash",20,"Male",101,29000);
-		}
+			for(int i = 0 ; i <= m - 1 ; i++){
+				emp.add(new employee("Shreyash",20,"Male",101,29000);
+			}
 
-		for(student st : stud){
-			st.display();
-		}
+			for(student st : stud){
+				st.display();
+			}
 
-		for(employee e : emp){
-			e.display();
+			for(employee e : emp){
+				e.display();
+			}
+		}catch(Exception e){
+			System.out.println(e);
+		}finally{
+			sc.close();
 		}
 	}
 }
