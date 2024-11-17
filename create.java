92ce3a2bafb073378548a12e1a2cf0c64dc2eccb
CodeAldous275 <aldousdsouza2@gmail.com>
2024-11-17 10:43:11 +0530

    New file added and updated a file

--- create.java
+++ create.java
@@ -1,7 +1,18 @@
+import java.util.*;
 public class create{
 	public static void main(String[] args){
-		student h = new student("Aldous",20,"Male","Science","Computer Science",2202301);
+		ArrayList<student> stud = new ArrayList<>();
 
-		h1.display();
+		System.out.print("Enter the no. of students:");
+		int n = sc.nextInt();
+
+		for(int i = 0 ; i <= n - 1 ; i++){
+			stud.add(new student("Aldous",20,"Male","Science","Computer Science",2202301);
+
+		}
+
+		for(student st : stud){
+			st.display();
+		}
 	}
 }
